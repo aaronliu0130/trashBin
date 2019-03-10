2f18af9cb247dd068ca3567e468913bd610213d5
Aaron Liu <aaronliu0130@gmail.com>
2019-03-10 20:16:04 +0800

    Update yuzu_easy_install_mac.sh
    Not tested yet.

--- yuzu_easy_install_mac.sh
+++ yuzu_easy_install_mac.sh
@@ -1,82 +1,90 @@
 #!/usr/bin/bash
+pause(){
+	read enterer
+	mytitle="Press Enter to Continue…"
+	echo -e '\033k'$mytitle'\033\\'
+}
+mytitle="Yuzu Keys Installer"
+echo -e '\033k'$mytitle'\033\\'
 clear
 menu=""
-while [[$menu!="n"]]||[[$menu!="y"]] do
+while [[$menu!="n"]]&&[[$menu!="y"]] do
 	echo -n "Do you want to update this script? (y/n): "
 	read -n 1 menu
 done
 if [[$menu=="y"]]; then
  	clear
-	echo "Updating. Will close after update."
-	update.bat
+	echo "Updating. Will restart after update."
+	curl -o update.sh https://raw.githubusercontent.com/aaronliu0130/trashBin/master/update.sh
+	bash update.sh
+	exit
+fi
+menu=""
+while [[$menu!="n"]]&&[[$menu!="y"]]&&[[$menu!="quit"]]
+	echo -n "Do you want to install Yuzu? (y/n/quit):  "
+	read menu
+done
+clear
 
-':A
-set /p menu="Do you want to install Yuzu? (Y/N/System Archives/Cancel): "
-if %menu%==Y goto Yes
-if %menu%==y goto Yes
-if %menu%==Yes goto Yes
-if %menu%==yes goto Yes
-if %menu%==N goto No
-if %menu%==n goto No
-if %menu%==No goto No
-if %menu%==no goto No
-if %menu%==C goto C
-if %menu%==c goto C
-if %menu%==Cancel goto C
-if %menu%==cancel goto C
-if %menu%==System Archives goto SA
-if %menu%==SA goto SA
-if %menu%==system archives goto SA
-if %menu%==sa goto SA
-cls
-set /p pause="Press any key to continue!... "
+if [[$menu==“y”]];then
+	clear
+	menu=""
+	while [[$menu!="c"]]&&[[$menu!="n"]] do
+		echo -n "Do you want to install Yuzu Canary or Nightly?Please use lowercase letters: "
+		read -n 1 menu
+	done
+	echo
+	echo "This will download Yuzu. Allow it to download."
+	echo
+	curl -o tar-archive-name.tar.gz -L 
+	echo We will now install yuzu, then delete the installer.
+	tar -zxvf tar-archive-name.tar.gz
+	echo -n "Done."
+	pause()
+fi
 
-:Yes
-cls
-echo.
-echo This will download the Yuzu installer, and run it. Allow it to install.
-echo.
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://www.dropbox.com/s/4vdziiwebovju8x/yuzu_install.exe?dl=1', 'yuzu_install.exe')
-echo We will now install yuzu, then delete the installer.
-yuzu_install.exe
-del yuzu_install.exe
-del yuzu_installer.log
-echo Done.
-pause
-
-:No
-cls
-echo Okay, that means it's time to download the keys.
-echo.
-echo We will now download the keys.
-cd %appdata%\yuzu
-mkdir keys
-cd keys
-echo Writing keys to %appdata%\yuzu\keys
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://s.hipeopeodev.us/prod.keys', 'prod.keys')
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://s.hipeopeodev.us/title.keys', 'title.keys')
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://s.hipeopeodev.us/shared_font.bin', 'shared_font.bin')
-pause
-
-:SA
-echo.
-echo We will now download the System Archives. This may take a while, it will be blank, but let it run.
-cd %appdata%\yuzu\nand\system
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://www.dropbox.com/s/0gwmpgus9t4q1dm/System_Archives.zip?dl=1', 'System_Archives.zip')
-echo unzipping System Archives.
-powershell.exe (new-object System.Net.WebClient).DownloadFile('http://stahlworks.com/dev/unzip.exe', 'unzip.exe')
-unzip.exe System_Archives.zip
-echo Writing System Archives to %appdata%\yuzu\keys\nand\system
-del System_Archives.zip
-del unzip.exe
-pause
-
+if [[$menu!="quit"]] then
+	clear
+	menu=""
+	while [[$menu!="y"]]&&[[$menu!="n"]] do
+		echo -n "Do you want to download the keys?(y/n): "
+		read -n 1 menu
+	done
+	if [[$menu=="y"]] then
+		echo
+		cd ~/.local/share/yuzu
+		mkdir keys
+		cd keys
+		echo "Writing keys to ~/.local/share/yuzu..."
+ 		curl -o prod.keys -L https://s.hipeopeodev.us/prod.keys
+		curl -o title.keys -L https://s.hipeopeodev.us/title.keys
+		curl -o shared_font.bin -L https://s.hipeopeodev.us/shared_font.bin
+		echo "Done."
+		pause()
+	fi
+	menu=""
+	while [[$menu!="y"]]&&[[$menu!="n]] do
+		echo -n "Do you want to install the System Archives?(y/n): "
+		read -n 1 menu
+	done
+	if [[$menu=="y"]] then
+		echo "We will now download the System Archives. This may take a while, let it download."
+		cd ~/.local/share/yuzu/nand/system
+		curl -o System_Archives.zip -L https://www.dropbox.com/s/0gwmpgus9t4q1dm/System_Archives.zip?dl=1
+		echo "Unzipping System Archives…"
+		unzip System_Archives.zip
+		echo "Writing System Archives to ~/.local/share/yuzu/nand/system"
+		del System_Archives.zip
+		echo -n "Done."
+		pause()
+	fi
+fi
 :C
-cls
-echo Thanks to /u/yuzu_pirate, /u/Azurime, and /u/bbb651 for their contributions to /r/YuzuP I R A C Y.
-echo.
-echo This program made by /u/Hipeopeo.
-echo.
-echo Thanks to the yuzu devs for making Yuzu!
-pause
-'
+clear
+echo "Thanks to /u/yuzu_pirate, /u/Azurime, and /u/bbb651 for their contributions to /r/YuzuP I R A C Y."
+echo
+echo "This program was originally made by /u/Hipeopeo, and ported to MacOS by /u/AaTube."
+echo
+echo "Thanks to the yuzu devs for making Yuzu!"
+pause()
+exit
