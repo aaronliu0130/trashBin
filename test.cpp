d905904401fccea00d1e1256b0a12375ee2fa315
Aaron Liu <aaronliu0130@gmail.com>
2019-08-24 09:39:58 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    /*
-     * @param start: a string
-     * @param end: a string
-     * @param dict: a set of string
-     * @return: An integer
-     */
-    int ladderLength(string &start, string &end, unordered_set<string> &dict) {
-        unordered_set<string>starts{start},ends{end};
-        auto grow=[&dict](unordered_set<string>& starts,unordered_set<string>& ends,int& times)->bool{
-            ++times;
-            unordered_set<string> ns;
-            for(auto s:starts)
-                for(auto&c:s){
-                    auto origi=c;
-                    for(c='a';c<='z';++c)
-                        if(c!=origi)
-                            if(ends.count(s))
-                                return false;
-                            else if(dict.count(s)){
-                                ns.insert(s);
-                                dict.erase(s);
-                            }
-                    c=origi;
-                }
-            starts=ns;
-            return true;
-        };
-        auto i=1;
-        while(grow(starts,ends,i)&&grow(ends,starts,i));
-        return i;
-    }
+    vector<vector<string>> findLadders(string &start, string &end,
+			unordered_set<string> &dict) {
+		unordered_map<string, unordered_set<string>> m;
+		unordered_set<string> s1 { start }, s2, *cur = &s1, *next = &s2;
+		dict.insert(end);
+		for (bool flag = 1; flag;) {
+			for (auto c : *cur) {
+				for (const auto& d : dict) {
+					auto diff = 0;
+					for (auto i = 0u; i < d.length(); i++)
+						if (d[i] != c[i] && ++diff > 1)
+							break;
+					if (diff == 1) {
+						m[d].insert(c);
+						next->insert(d);
+						if (d == end)
+							flag = 0;
+					}
+				}
+			}
+			if (flag = flag && !next->empty()) {
+				swap(cur, next);
+				next->clear();
+				for (auto c : *cur)
+					dict.erase(c);
+			}
+		}
+		vector<vector<string>> result;
+		function<vector<vector<string>>(string)> dfs;
+		dfs = [&dfs,&m,&start](string s)->vector<vector<string>> {
+			vector<vector<string>> ans;
+			if(s==start)return { {start}};
+			for(auto i:m[s])
+			for(auto d:dfs(i)) {
+				ans.push_back(d);
+				ans.back().push_back(s);
+			}
+			return ans;
+		};
+		return dfs(end);
+	}
 };
