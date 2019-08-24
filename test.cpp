0334270aae29941ba94f14cc7a09ef218cb4f4a4
Aaron Liu <aaronliu0130@gmail.com>
2019-08-24 08:46:39 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,32 @@
+	vector<vector<int>> permuteUnique(vector<int> nums) {
+		// write your code here
+		int s = nums.size();
+		if (s < 1)
+			return { {}};
+		vector<int> states(s, 0);
+		vector<bool> used(s + 1, 0);
+		vector<vector<int>> ans;
+		sort(nums.begin(), nums.end());
+		nums.insert(nums.begin(), INT_MIN);
+		for (int i = 0; i >= 0;) {
+			auto prev = states[i];
+			auto flag = 0;
+			for (used[prev] = 0; !flag && ++states[i] <= s;
+					flag = !used[states[i]] && nums[states[i]] != nums[prev])
+				;
+			if (flag) {
+				used[states[i]] = 1;
+				++i;
+				if (i >= s) {
+					vector<int> a(s);
+					for (int j = 0; j < s; ++j)
+						a[j] = nums[states[j]];
+					ans.push_back(a);
+					--i;
+				} else
+					states[i] = 0;
+			} else
+				--i;
+		}
+		return ans;
+	}
